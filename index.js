0434c6c1ccb3af9c6f48dc288ce5a0589836111c
nghiattbss <nghiatt@bsscommerce.com>
2025-08-27 10:49:33 +0700

    update demo

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import fs from 'fs';
 import {PNG} from 'pngjs';
 import pixelmatch from 'pixelmatch';
 
-const img1 = PNG.sync.read(fs.readFileSync('files/1.png'));
-const img2 = PNG.sync.read(fs.readFileSync('files/2.png'));
+const img1 = PNG.sync.read(fs.readFileSync('files/6a.png'));
+const img2 = PNG.sync.read(fs.readFileSync('files/6b.png'));
 const {width, height} = img1;
 const diff = new PNG({width, height});
-
-console.log(width, height);
 
 pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.3});
 
